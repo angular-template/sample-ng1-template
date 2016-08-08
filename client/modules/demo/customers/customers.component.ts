a6bd8417bf2a2da225b054ceadd1dea9ecaa922f
Jeevan James <jeevanjj@yahoo.com>
2016-08-09 00:00:06 +0530

    Updates to ng1-template-gulp and ng1-template-core packages.

--- client/modules/demo/customers/customers.component.ts
+++ client/modules/demo/customers/customers.component.ts
@@ -11,17 +11,44 @@ namespace demo.customers {
         selector: name
     }, {
         path: '/demos/customers',
-        parent: demo.layouts.sample.name
+        parent: demo.layouts.sample.name,
+        params: {
+            cust: new Date()
+        }
     })
     export class CustomersComponent implements ng.IComponentController {
         /* @ngInject */
-        constructor(private customersWebService: ws.CustomersWebService) {
+        constructor(
+            private customersWebService: ws.CustomersWebService,
+            private $stateParams: ng.ui.IStateParamsService,
+            private customersParams: CustomerParams
+        ) {
         }
 
         public $onInit(): void {
-            this.customers = this.customersWebService.getCustomers();
+            this.customers = this.customersWebService.getCustomers()
+                .concat(this.$stateParams['cust'] || [
+                    {
+                        id: 77,
+                        firstName: 'Jeevan',
+                        lastName: 'James',
+                        startDate: new Date()
+                    }
+                ])
+                .concat({
+                    id: 567,
+                    firstName: this.customersParams.firstName || 'Not assigned',
+                    lastName: 'James',
+                    startDate: new Date()
+                });
         }
 
         public customers: wsModels.Customer[];
+    }
+
+    @InjectableState('customersParams')
+    export class CustomerParams {
+        @state.session()
+        public firstName: string;
     }
 }
--- client/modules/demo/home/home.component.ts
+++ client/modules/demo/home/home.component.ts
@@ -11,5 +11,21 @@ namespace demo.home {
             parent: layouts.sample.name
         })
     export class HomeComponent implements ng.IComponentController {
+        /* @ngInject */
+        constructor(private $state: ng.ui.IStateService, private customersParams: customers.CustomerParams) {
+        }
+
+        public $onInit(): void {
+            this.fname = this.customersParams.firstName;
+        }
+
+        public fname: string = 'Newer FF Name';
+
+        public showCustomers(): void {
+            this.customersParams.firstName = this.fname;
+            this.$state.go(customers.name, {
+                cust: { id: 82, firstName: 'Anders', lastName: 'Heijlsberg', startDate: new Date() }
+            });
+        }
     }
 }
